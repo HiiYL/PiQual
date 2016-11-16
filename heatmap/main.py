9a4eb5b1224a79c686351e6d7d95d6151a995acb
HiiYL <yonglian146@gmail.com>
2016-11-16 14:38:14 +0800

    Added heatmap

---
+++ heatmap/main.py
@@ -0,0 +1,81 @@
+def VGG_19(weights_path=None,heatmap=False):
+    model = Sequential()
+
+    if heatmap:
+        model.add(ZeroPadding2D((1,1),input_shape=(3,None,None)))
+    else:
+        model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_4'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_4'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_4'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    if heatmap:
+        model.add(Convolution2D(4096,7,7,activation="relu",name="dense_1"))
+        model.add(Convolution2D(4096,1,1,activation="relu",name="dense_2"))
+        model.add(Convolution2D(1000,1,1,name="dense_3"))
+        model.add(Softmax4D(axis=1,name="softmax"))
+    else:
+        model.add(Flatten())
+        model.add(Dense(4096, activation='relu', name='dense_1'))
+        model.add(Dropout(0.5))
+        model.add(Dense(4096, activation='relu', name='dense_2'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1000, name='dense_3'))
+        model.add(Activation("softmax"))
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
+
+if __name__ == "__main__":
+    # ### Here is a script to compute the heatmap of the dog synsets.
+    # ## We find the synsets corresponding to dogs on ImageNet website
+    # s = "n02084071"
+    # ids = synset_to_dfs_ids(s)
+    # # Most of the synsets are not in the subset of the synsets used in ImageNet recognition task.
+    # ids = np.array([id_ for id_ in ids if id_ is not None])
+
+    im = preprocess_image_batch(['examples/dog.jpg'], color_mode="rgb")
+
+    # Test pretrained model
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model = convnet('alexnet',weights_path="weights/alexnet_weights.h5", heatmap=True)
+    model.compile(optimizer=sgd, loss='mse')
