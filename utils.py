479e5d4259dc9eb91cc52dad301e130788d6adc5
HiiYL <yonglian146@gmail.com>
2016-11-23 12:17:04 +0800

    Added aspect ratio calculator

--- utils.py
+++ utils.py
@@ -18,3 +18,17 @@ def copyTestSet(dest_dir="test_images/"):
     filepath = os.path.join(ava_data_path, filename)
     copyfile(filepath, dest_dir + filename)
     i=i+1
+
+
+
+def computeAspectRatio():
+  store = HDFStore('dataset_h5/labels.h5')
+  ava_table = store['labels_train']
+  aspect_ratio_list = []
+  for index in ava_table.index:
+    if (i % 1000) == 0:
+      print('Now Processing {0}/{1}'.format(i,periodNum))
+    filename = str(index) + ".jpg"
+    filepath = os.path.join(self.ava_data_path, filename)
+    height, width, channels = cv2.imread(filepath).shape
+    aspect_ratio = float("{0:.2f}".format(height/width)
