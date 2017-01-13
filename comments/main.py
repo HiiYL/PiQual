b2497382faf462d3141fffbcb01ac127ed70d92a
HiiYL <yonglian146@gmail.com>
2017-01-13 13:38:39 +0800

    Added 2 layer gru

--- comments/main.py
+++ comments/main.py
@@ -71,6 +71,37 @@ def generateIndexMappingToEmbedding():
   return embeddings_index
   
 
+def getDistribution(dataframe):
+    ratings_matrix = dataframe.ix[:,:10]
+    sum_of_ratings = (dataframe.ix[:,:10]).sum(axis=1)
+    normalized_score_distribution = ratings_matrix.div(sum_of_ratings,axis='index')
+    return normalized_score_distribution.as_matrix()
+
+
+
+def evaluate_distribution_accuracy(model, X_test, Y_test):
+    y_pred = model.predict(X_test)
+
+    if(y_pred is list):
+        aesthetics_pred = y_pred[0]
+    else:
+        aesthetics_pred = y_pred
+
+
+    weights = np.array([1,2,3,4,5,6,7,8,9,10])
+
+    score_pred = (aesthetics_pred * weights).sum(axis=1)
+    score_test = (Y_test * weights).sum(axis=1)
+
+    Y_pred_binary = np.array([ 1 if row >= 5 else 0 for row in score_pred])
+    Y_test_binary = np.array([ 1 if row >= 5 else 0 for row in score_test])
+
+
+    accuracy = np.sum(Y_pred_binary == Y_test_binary) / len(Y_test_binary)
+
+    print("accuracy = {} %".format(accuracy * 100))
+    return accuracy
+
 
 store = HDFStore('../dataset_h5/labels.h5')
 
@@ -81,16 +112,19 @@ ava_table = ava_table.sort_values(by="score")
 comments_train = ava_table.ix[:,'comments'].as_matrix()
 
 
-Y_train = ava_table.ix[:, "good"].as_matrix()
-Y_train = to_categorical(Y_train, 2)
+# Y_train = ava_table.ix[:, "good"].as_matrix()
+# Y_train = to_categorical(Y_train, 2)
 
+Y_train = getDistribution(ava_table)
 
 
 ava_test = store['labels_test']
 comments_test = ava_test.ix[:,'comments'].as_matrix()
 
-Y_test = ava_test.ix[:, "good"].as_matrix()
-Y_test = to_categorical(Y_test, 2)
+# Y_test = ava_test.ix[:, "good"].as_matrix()
+# Y_test = to_categorical(Y_test, 2)
+
+Y_test = getDistribution(ava_test)
 
 X_train, X_test, word_index = tokenizeAndGenerateIndex(comments_train, comments_test)
 
@@ -110,40 +144,51 @@ embedding_layer = Embedding(len(word_index) + 1,
                             input_length=maxlen,
                             trainable=False, name="embedding")
 
-comment_input = Input(shape=(100,), dtype='int32')
-embedded_sequences = embedding_layer(comment_input)
-# x = GRU(EMBEDDING_DIM)(embedded_sequences) # 0.8013
-# x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3)(embedded_sequences) #0.8109
+# comment_input = Input(shape=(100,), dtype='int32')
+# embedded_sequences = embedding_layer(comment_input)
+# # x = GRU(EMBEDDING_DIM)(embedded_sequences) # 0.8013
+# # x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3)(embedded_sequences) #0.8109
 
 
-# x = Convolution1D(128, 5, activation='relu')(embedded_sequences)
-# x = MaxPooling1D(5)(x)
-# x = Convolution1D(128, 5, activation='relu')(x)
-# x = MaxPooling1D(5)(x)
-# x = Convolution1D(128, 5, activation='relu')(x)
-# x = MaxPooling1D(35)(x)  # global max pooling
-x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3, name="gru")(embedded_sequences)
-# x = Flatten()(x)
-# x = Dense(128, activation='relu')(x)
-# x = Dropout(0.5)(x)
+# # x = Convolution1D(128, 5, activation='relu')(embedded_sequences)
+# # x = MaxPooling1D(5)(x)
+# # x = Convolution1D(128, 5, activation='relu')(x)
+# # x = MaxPooling1D(5)(x)
+# # x = Convolution1D(128, 5, activation='relu')(x)
+# # x = MaxPooling1D(35)(x)  # global max pooling
+# # x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3,return_sequences=True, name="gru")(embedded_sequences)
+# # # x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3,return_sequences=True, name="gru_2")(x)
+# # # x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3,return_sequences=True, name="gru_3")(x)
+# # x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3, name="gru_2")(x)
+# # x = Flatten()(x)
+# # x = Dense(128, activation='relu')(x)
+# # x = Dropout(0.5)(x)
 
-# x = Flatten()(embedded_sequences)
-preds = Dense(2, name="comment_output",activation='softmax')(x)
+# # x = Flatten()(embedded_sequences)
 
-# question_input = Input(shape=(maxlen,), dtype='int32')
-# x = Embedding(input_dim=max_features,
-#  output_dim=EMBEDDING_DIM, input_length=maxlen,
-#    dropout=0.25)(question_input)
-# # x = Convolution1D(nb_filter=nb_filter,
-# #                         filter_length=filter_length,
-# #                         border_mode='valid',
-# #                         activation='relu',
-# #                         subsample_length=1)(x)
-# # x = MaxPooling1D(pool_length=pool_length)(x)
-# x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3)(x)
-# output = Dense(2, activation='softmax')(x)
+# x = LSTM(EMBEDDING_DIM,batch_input_shape=(128, 1, 1),dropout_W = 0.3,dropout_U = 0.3,return_sequences=True, name="lstm",stateful=True)(embedded_sequences)
+# # x = LSTM(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3,return_sequences=True, name="lstm_2")(x)
+# # x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3,return_sequences=True, name="gru_3")(x)
+# x = LSTM(EMBEDDING_DIM,batch_input_shape=(128, 1, 1),dropout_W = 0.3,dropout_U = 0.3, name="lstm_2",stateful=True)(x)
 
-model = Model(input=comment_input, output=preds)
+
+
+# preds = Dense(10, name="comment_output",activation='softmax')(x)
+
+# # question_input = Input(shape=(maxlen,), dtype='int32')
+# # x = Embedding(input_dim=max_features,
+# #  output_dim=EMBEDDING_DIM, input_length=maxlen,
+# #    dropout=0.25)(question_input)
+# # # x = Convolution1D(nb_filter=nb_filter,
+# # #                         filter_length=filter_length,
+# # #                         border_mode='valid',
+# # #                         activation='relu',
+# # #                         subsample_length=1)(x)
+# # # x = MaxPooling1D(pool_length=pool_length)(x)
+# # x = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3)(x)
+# # output = Dense(2, activation='softmax')(x)
+
+# model = Model(input=comment_input, output=preds)
 
 # model.load_weights('text_binary_weights2016-12-07 09:31:43.h5')
 
@@ -151,18 +196,32 @@ model = Model(input=comment_input, output=preds)
 # sgd = SGD(lr=0.01, decay=5e-4, momentum=0.9, nesterov=True, clipnorm=1., clipvalue=0.5)
 
 
-time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-checkpointer = ModelCheckpoint(filepath="text_binary_weights{}.h5".format(time_now), verbose=1, save_best_only=True)
+
+
+model = Sequential()
+model.add(LSTM(EMBEDDING_DIM, batch_input_shape=(128, 100, 1), return_sequences=True, stateful=True))
+model.add(LSTM(EMBEDDING_DIM, batch_input_shape=(128, 100, 1), return_sequences=False, stateful=True))
+model.add(Dense(2, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model_identifier = "text_stateful_distribution"
+# time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+checkpointer = ModelCheckpoint(filepath="weights/{} - {}.h5".format(time_now, model_identifier), verbose=1, save_best_only=True)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,patience=2)
-csv_logger = CSVLogger('training_binary_text{}.log'.format(time_now))
+csv_logger = CSVLogger('logs/{} - {}.log'.format(time_now,model_identifier))
 
 
-model.compile(loss='categorical_crossentropy',
+# model.load_weights("weights/{} - {}.h5".format("2017-01-06 00:03:26",model_identifier))
+
+model.compile(loss='kld',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
 
 model.fit(X_train, Y_train,
           batch_size=128,
-          nb_epoch=20,
+          nb_epoch=10,
           validation_data=(X_test, Y_test), callbacks=[checkpointer, reduce_lr, csv_logger])
+
+
+accuracy = evaluate_distribution_accuracy(model, X_test, Y_test)
--- models.py
+++ models.py
@@ -1,286 +1,13 @@
-from googlenet_custom_layers import PoolHelper,LRN
-from keras.models import model_from_json
-import cv2
-
-from scipy import ndimage, misc
-import cv2
-import numpy as np
-import os
-import pickle
-import pandas as pd
-from pandas import HDFStore, DataFrame
-
-import h5py
-import matplotlib.pyplot as plt
-
-import sys
-sys.setrecursionlimit(10000)
-
-from scipy.misc import imread, imresize
-
-from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D, Dropout, Flatten, merge, Reshape, Activation
-from keras.layers import Input, Embedding, LSTM, Dense, Activation, GRU,Convolution1D,Dropout
-from keras.layers.pooling import GlobalAveragePooling2D, GlobalMaxPooling2D,GlobalMaxPooling1D
-from keras.models import Model
-from keras.regularizers import l2
-from keras.optimizers import SGD, RMSprop
-from keras.utils.np_utils import to_categorical
-from keras.callbacks import CSVLogger, ReduceLROnPlateau, ModelCheckpoint
-
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-
-
-from googlenet_custom_layers import PoolHelper,LRN
-
-from datetime import datetime
-
-
-
-max_features = 20000
-maxlen=100
-EMBEDDING_DIM = 300
-
-
-GLOVE_DIR = "../comments/glove/"
-
-def tokenizeAndGenerateIndex(train, test):
-  merged = np.concatenate([train, test])
-  tokenizer = Tokenizer(nb_words=max_features)
-  tokenizer.fit_on_texts(merged)
-  sequences_train = tokenizer.texts_to_sequences(train)
-  sequences_test = tokenizer.texts_to_sequences(test)
-  word_index = tokenizer.word_index
-  print('Found %s unique tokens.' % len(word_index))
-  data_train = pad_sequences(sequences_train, maxlen=maxlen)
-  data_test = pad_sequences(sequences_test, maxlen=maxlen)
-  return data_train, data_test, word_index
-
-
-def generateIndexMappingToEmbedding():
-  embeddings_index = {}
-  f = open(os.path.join(GLOVE_DIR, 'glove.6B.{}d.txt'.format(EMBEDDING_DIM)))
-  for line in f:
-      values = line.split()
-      word = values[0]
-      coefs = np.asarray(values[1:], dtype='float32')
-      embeddings_index[word] = coefs
-  f.close()
-
-  print('Found %s word vectors.' % len(embeddings_index))
-  return embeddings_index
-
-def process_image(image):
-    im = np.copy(image)
-    im[:,:,0] -= 103.939
-    im[:,:,1] -= 116.779
-    im[:,:,2] -= 123.68
-    im = im.transpose((2,0,1))
-    im = np.expand_dims(im, axis=0)
-    return im
-
-def deprocess_image(image):
-    im = np.copy(image)
-    im[:,:,0] += 103.939
-    im[:,:,1] += 116.779
-    im[:,:,2] += 123.
-
-    im = im.transpose((1,2,0))
-
-    return im
-
-def read_and_generate_heatmap(input_path, output_path):
-    original_img = cv2.imread(input_path).astype(np.float32)
-
-    width, height, _ = original_img.shape
-
-    im = process_image(cv2.resize(original_img,(224,224)))
-    out = model.predict(im)
-
-    class_weights = model.layers[-1].get_weights()[0]
-    print("predictions", out[0])
-
-    conv_output = out[2][0,:,:,:]
-    #Create the class activation map.
-    cam = np.zeros(dtype = np.float32, shape = conv_output.shape[1:3])
-
-    class_to_visualize = 1 # 0 for bad, 1 for good
-    for i, w in enumerate(class_weights[:, class_to_visualize]):
-            cam += w * conv_output[i, :, :]
-
-    cam /= np.max(cam)
-    cam = cv2.resize(cam, (height, width))
-    heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
-    heatmap[np.where(cam < 0.2)] = 0
-    temp = heatmap*0.5 + original_img
-    file_name = input_path.split("/")[-1].split(".jpg")[0]
-
-    cv2.imwrite('output/' + input_path.split("/")[-1],original_img)
-    cv2.imwrite('output/{}_aesthetics.jpg'.format(file_name), temp)
-
-
-    top_3_hits = np.argsort(out[1], axis=1)[0][::-1][:3]
-    print("predictions", top_3_hits)
-    semantic_tags = [ semantics.ix[hit + 1].semantic[1:] for hit in top_3_hits] ##NOTE: Quick fix to remove space 
-
-
-    conv_output = out[3][0,:,:,:]
-    #Create the class activation map.
-    nth_top_semantic = 0
-
-    for nth_top_semantic in range(len(top_3_hits)):
-        cam = np.zeros(dtype = np.float32, shape = conv_output.shape[1:3])
-        class_to_visualize = top_3_hits[nth_top_semantic] # 0 for bad, 1 for good
-        for i, w in enumerate(class_weights[:, class_to_visualize]):
-                cam += w * conv_output[i, :, :]
-                
-        cam /= np.max(cam)
-        cam = cv2.resize(cam, (height, width))
-        heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
-        heatmap[np.where(cam < 0.2)] = 0
-        temp = heatmap*0.5 + original_img
-        file_name = input_path.split("/")[-1].split(".jpg")[0]
-        cv2.imwrite('output/{}_{}.jpg'.format(file_name, semantic_tags[nth_top_semantic]), temp)
-
-
-def generate_images_heatmap(model, ava_table, style_category=-1):
-    input_images_dir = "images/"
-    output_dir = "output/"
-
-    semantics = pd.read_table('../dataset/AVA/tags.txt',delimiter="(\d+)",
-     usecols=[1,2], index_col=0, header=None,names=['index','semantic'])
-
-    if style_category != -1:
-        style = pd.read_table('../dataset/AVA/style_image_lists/train.jpgl', index_col=0)
-        tag = pd.read_table('../dataset/AVA/style_image_lists/train.lab')
-
-        style.loc[:,'style'] = tag.as_matrix()
-        ava_with_style = style.join(ava_table, how='inner')
-
-        images_with_style = ava_with_style.ix[(ava_with_style.ix[:,'style'] == style_category)]
-        images_with_style = images_with_style.sort_values(by="score")
-
-        ava_path = "../dataset/AVA/data/"
-        for index in images_with_style.iloc[::-1][:25].index:
-            image_name = str(index) + ".jpg"
-            input_path = ava_path + image_name
-            output_path = output_dir + image_name
-            read_and_generate_heatmap(input_path, output_path)
-    else:
-        for file in os.listdir(input_images_dir):
-            if file.endswith('.jpg'):
-                read_and_generate_heatmap(input_images_dir + file, output_dir + file)
-
-
-def getDistribution(dataframe):
-    ratings_matrix = dataframe.ix[:,:10]
-    sum_of_ratings = (dataframe.ix[:,:10]).sum(axis=1)
-    normalized_score_distribution = ratings_matrix.div(sum_of_ratings,axis='index')
-    return normalized_score_distribution.as_matrix()
-
-
-def prepareData(delta=0.0, use_distribution=False, use_semantics=False, use_comments=False):
-    store = HDFStore('../dataset_h5/labels.h5','r')
-    ava_table = store['labels_train']
-    ava_test = store['labels_test']
-
-    if delta > 0.0:
-        ava_table = ava_table[( abs(ava_table.score - 5) >= delta)]
-
-
-    h5f = h5py.File('../dataset_h5/images_224_delta_{0}.h5'.format(delta),'r')
-    X_train = h5f['data_train']
-    X_test = h5f['data_test']
-
-    if use_comments:
-        comments_train = ava_table.ix[:,'comments'].as_matrix()
-        comments_test = ava_test.ix[:,'comments'].as_matrix()
-
-        X_train_text, X_test_text, word_index = tokenizeAndGenerateIndex(comments_train, comments_test)
-
-        embeddings_index = generateIndexMappingToEmbedding()
-        embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
-        for word, i in word_index.items():
-            embedding_vector = embeddings_index.get(word)
-            if embedding_vector is not None:
-                # words not found in embedding index will be all-zeros.
-                embedding_matrix[i] = embedding_vector
-
-        embedding_layer = Embedding(len(word_index) + 1,
-                                    EMBEDDING_DIM,
-                                    weights=[embedding_matrix],
-                                    input_length=maxlen,
-                                    trainable=False)
-
-    if use_distribution:
-        Y_train = getDistribution(ava_table)
-        Y_test = getDistribution(ava_test)
-    else:
-        Y_train = ava_table.ix[:, "good"].as_matrix()
-        Y_train = to_categorical(Y_train, 2)
-
-        Y_test = ava_test.ix[:, "good"].as_matrix()
-        Y_test = to_categorical(Y_test, 2)
-
-    if use_semantics:
-        Y_train_semantics = to_categorical(ava_table.ix[:,10:12].as_matrix())[:,1:]
-        Y_test_semantics = to_categorical(ava_test.ix[:,10:12].as_matrix())[:,1:]
-
-        if use_comments:
-            return X_train, Y_train, Y_train_semantics, X_test, Y_test, Y_test_semantics, X_train_text, X_test_text, embedding_layer
-        else:
-            return X_train, Y_train, Y_train_semantics, X_test, Y_test, Y_test_semantics
-
-
-    if use_comments:
-        return X_train, Y_train, X_test, Y_test, X_train_text, X_test_text, embedding_layer
-    else:
-        return X_train, Y_train, X_test, Y_test
-
-
-def evaluate_distribution_accuracy(model, X_test, Y_test):
-    y_pred = model.predict(X_test)
-
-    if(isinstance(y_pred,list)):
-        aesthetics_pred = np.array(y_pred[0])
-    else:
-        aesthetics_pred = np.array(y_pred)
-
-
-    weights = np.array([1,2,3,4,5,6,7,8,9,10])
-
-    score_pred = (aesthetics_pred * weights).sum(axis=1)
-    score_test = (Y_test * weights).sum(axis=1)
-
-    Y_pred_binary = np.array([ 1 if row >= 5 else 0 for row in score_pred])
-    Y_test_binary = np.array([ 1 if row >= 5 else 0 for row in score_test])
-
-
-    accuracy = np.sum(Y_pred_binary == Y_test_binary) / len(Y_test_binary)
-
-    print("accuracy = {} %".format(accuracy * 100))
-    return accuracy
-
-
-
 def create_googlenet(weights_path=None, use_distribution=False, use_multigap=False,use_semantics=False, use_comments=False, embedding_layer=None,heatmap=False):
 
     input_image = Input(shape=(3, 224, 224))
 
-    if use_comments:
-        if embedding_layer is None:
-            print("[ERROR] Embedding layer is required for creating comments model")
-            return None
-
+    if embedding_layer and use_comments:
         comment_input = Input(shape=(maxlen,), dtype='int32')
         embedded_sequences = embedding_layer(comment_input)
         
-        x_text_aesthetics = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3,return_sequences=True, name='gru_aesthetics_1')(embedded_sequences)
-        x_text_aesthetics = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3,name='gru_aesthetics_2')(x_text_aesthetics)
-
-        x_text_semantics = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3,return_sequences=True, name='gru_semantics_1')(embedded_sequences)
-        x_text_semantics = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3, name='gru_semantics_2')(x_text_semantics)
-
+        x_text_aesthetics = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3)(embedded_sequences)
+        x_text_semantics = GRU(EMBEDDING_DIM,dropout_W = 0.3,dropout_U = 0.3)(embedded_sequences)
     
     conv1_7x7_s2 = Convolution2D(64,7,7,subsample=(2,2),border_mode='same',activation='relu',name='conv1/7x7_s2',W_regularizer=l2(0.0002))(input_image)
     conv1_zero_pad = ZeroPadding2D(padding=(1, 1))(conv1_7x7_s2)
@@ -383,10 +110,6 @@ def create_googlenet(weights_path=None, use_distribution=False, use_multigap=Fal
         conv_output_semantics = Convolution2D(1024, 3, 3, activation='relu',name='conv_6_1_semantics',border_mode = 'same',W_regularizer=l2(0.0002))(inception_4e_output_semantics)
         
         x_semantics = GlobalAveragePooling2D()(conv_output_semantics)
-
-        if use_comments:
-             x_semantics = merge([x_semantics, x_text_semantics],mode='concat',concat_axis=1)
-
         output_semantics = Dense(65, activation = 'softmax', name="output_semantics")(x_semantics)
 
     else:
@@ -403,10 +126,7 @@ def create_googlenet(weights_path=None, use_distribution=False, use_multigap=Fal
     x_aesthetics = GlobalAveragePooling2D()(conv_output_aesthetics)
 
     if use_multigap:
-        if use_comments:
-            x_aesthetics = merge([x_aesthetics, x_text_aesthetics, inception_4a_gap, inception_4b_gap, inception_4c_gap, inception_4d_gap] ,mode='concat',concat_axis=1)
-        else:
-            x_aesthetics = merge([x_aesthetics, inception_4a_gap, inception_4b_gap, inception_4c_gap, inception_4d_gap] ,mode='concat',concat_axis=1)
+        x_aesthetics = merge([x_aesthetics, inception_4a_gap, inception_4b_gap, inception_4c_gap, inception_4d_gap] ,mode='concat',concat_axis=1)
 
     if use_distribution:
         if use_multigap:
@@ -420,76 +140,17 @@ def create_googlenet(weights_path=None, use_distribution=False, use_multigap=Fal
             output_aesthetics = Dense(2, activation = 'softmax', name="main_output")(x_aesthetics)
     
     if use_semantics:
-        if use_comments:
+        if embedding_layer and use_comments:
             googlenet = Model(input=[input_image, comment_input], output=[output_aesthetics,output_semantics])
         else:
             googlenet = Model(input=input_image, output=[output_aesthetics,output_semantics])
     else:
-        if use_comments:
+        if embedding_layer and use_comments:
             googlenet = Model(input=[input_image, comment_input], output=output_aesthetics)
         else:
             googlenet = Model(input=input_image, output=output_aesthetics)
     
     if weights_path:
-        if use_semantics:
-            googlenet.load_weights('weights/named_googlenet_semantics_weights.h5', by_name=True)
         googlenet.load_weights(weights_path,by_name=True)
-
     
     return googlenet
-
-
-use_distribution = True
-use_semantics = False
-# X_train, Y_train, X_test, Y_test = prepareData(use_distribution=use_distribution)
-X_train, Y_train,X_test, Y_test,X_train_text, X_test_text,embedding_layer= prepareData(use_distribution=use_distribution, use_semantics=use_semantics, use_comments=True)
-
-model = create_googlenet('weights/googlenet_aesthetics_weights.h5',
- use_distribution=use_distribution, use_semantics=use_semantics,use_multigap=True,use_comments=True,
-  embedding_layer=embedding_layer, heatmap=False)
-# sgd = SGD(lr=0.001, decay=5e-4, momentum=0.9, nesterov=True)
-rmsProp = RMSprop(lr=0.00001, rho=0.9, epsilon=1e-08, decay=0.0)
-
-if use_distribution:
-    print("using kld loss...")
-    model.compile(optimizer=rmsProp,loss='kld', metrics=['accuracy'])#,loss_weights=[1., 0.2])
-else:
-    print("using categorical crossentropy loss...")
-    model.compile(optimizer=rmsProp,loss='categorical_crossentropy', metrics=['accuracy'])#,loss_weights=[1., 0.2])
-
-
-time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-model_identifier = "distribution_2layer_gru"
-unique_model_identifier = "{} - {}".format(time_now, model_identifier)
-
-checkpointer = ModelCheckpoint(filepath="weights/{}.h5".format(unique_model_identifier), verbose=1, save_best_only=True)
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,patience=2)
-csv_logger = CSVLogger('logs/{}.log'.format(unique_model_identifier))
-
-# class_weight = {0 : 0.67, 1: 0.33}
-# model.fit(X_train,Y_train,nb_epoch=20, batch_size=32, shuffle="batch",
-#  validation_data=(X_test, Y_test), callbacks=[csv_logger,checkpointer,reduce_lr])#,reduce_lr])#,class_weight = class_weight)
-
-model.fit([X_train,X_train_text],Y_train,
-    nb_epoch=20, batch_size=32, shuffle="batch",
-    validation_data=([X_test,X_test_text], Y_test),
-    callbacks=[csv_logger,checkpointer,reduce_lr])#,reduce_lr])#,class_weight = class_weight)
-
-from keras.utils.visualize_util import plot
-plot(model, to_file='{}.png'.format(unique_model_identifier),show_shapes=True)
-
-model = create_googlenet('weights/gap_distribution_weights_aes_only_sgd2016-12-31 14:53:03.h5',
- use_distribution=use_distribution,use_multigap=True,heatmap=False)
-
-model.evaluate(X_test,Y_test)
-accuracy = evaluate_distribution_accuracy(model, [X_test,X_test_text], Y_test)
-
-
-
-
-
-
-
-
-
