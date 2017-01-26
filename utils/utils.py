ec9cd6814cc7826eb88b539ec54aa12d3d43f8dd
HiiYL <yonglian146@gmail.com>
2017-01-26 09:49:45 +0800

    did not push updates to file in subdir

--- utils/utils.py
+++ utils/utils.py
@@ -8,14 +8,14 @@ import cv2
 import numpy as np
 
 def copyTestSet(dest_dir="test_images/"):
-  ava_path = "dataset/AVA/data/"
+  ava_path = "../dataset/AVA/data/"
   ava_data_path = os.path.join(os.getcwd(), ava_path)
   store = HDFStore('dataset_h5/labels.h5')
   test_labels = store['labels_test']
   imagesCount = test_labels.shape[0]
 
   i = 0
-  for index, row in test_labels.iterrows():
+  for index in test_labels.index:
     if (i % 1000) == 0:
       print("Now processing {} / {}".format(i,imagesCount))
     filename = str(index) + ".jpg"
